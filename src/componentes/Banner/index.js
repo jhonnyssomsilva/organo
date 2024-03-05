e8c125d363160c0832628929aa180a002d8f7503
Jhonnyssom <jhonnyssomux@gmail.com>
2024-03-05 15:25:25 -0300

    primeiros `componentes`

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+function Banner() {
+  //JSX
+  return (
+    <header className="banner">
+      <img
+        src="./assets/banner.png"
+        alt="O banner principal da pagina do Organo"
+      />
+    </header>
+  );
+}
+export default Banner;
