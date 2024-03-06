79047efdf1a55c9fc7cc5142c3e26acaaeb082ec
Jhonnyssom <jhonnyssomux@gmail.com>
2024-03-06 10:16:03 -0300

    add campo de `obrigatoriedade`

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -13,15 +13,27 @@ const Formulario = () => {
     "Mobile",
     "Inovação e Gestão",
   ];
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Formulário submetido!");
+  };
 
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite o seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite o seu Cargo"
+        />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
         <Botao>Criar Card</Botao>
       </form>
     </section>
