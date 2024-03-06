4bd512948b8a5618d1eeafca6df1442d0776e499
Jhonnyssom <jhonnyssomux@gmail.com>
2024-03-06 15:10:44 -0300

    salvando os input

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import Botao from "../Botão";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-const Formulario = () => {
+const Formulario = (props) => {
   const times = [
     "Programação",
     "Front-End",
@@ -13,9 +14,20 @@ const Formulario = () => {
     "Mobile",
     "Inovação e Gestão",
   ];
+
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Formulário submetido!");
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
   };
 
   return (
@@ -26,14 +38,30 @@ const Formulario = () => {
           obrigatorio={true}
           label="Nome"
           placeholder="Digite o seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Cargo"
           placeholder="Digite o seu Cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
         />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Informe o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
         <Botao>Criar Card</Botao>
       </form>
     </section>
